397c7072948ca59086c5c2ee0d221c1b442df584
senna-lang <asrsena3302@gmail.com>
2024-02-14 10:47:43 +0900

    非同期テスト

--- src/components/skills/Skill.test.tsx
+++ src/components/skills/Skill.test.tsx
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import Skills from './Skills';
 
 describe('Skills', () => {
-
   const skills = ['HTML', 'CSS', 'Javascript'];
 
   test('renders correctly', () => {
@@ -15,5 +14,29 @@ describe('Skills', () => {
     render(<Skills skills={skills} />);
     const listElements = screen.getAllByRole('listitem');
     expect(listElements).toHaveLength(skills.length);
+  });
+
+  test('renders Login button', () => {
+    render(<Skills skills={skills} />);
+    const loginButton = screen.getByRole('button', {
+      name: 'ログイン',
+    });
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  test('logout button is not rendered', () => {
+    render(<Skills skills={skills} />);
+    const logoutButton = screen.queryByRole('button', {
+      name: 'ログアウト',
+    });
+    expect(logoutButton).not.toBeInTheDocument();
+  });
+
+  test('logout button is eventually rendered', async() => {
+    render(<Skills skills={skills} />);
+    const logoutButton =await screen.findByRole('button', {
+      name: 'ログアウト',
+    },);
+    expect(logoutButton).toBeInTheDocument();
   });
 });
--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import { SkillProps } from './Skills.types';
 
 const Skills = (props: SkillProps) => {
   const { skills } = props;
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+   setTimeout(() => {
+      setIsLogin(true)
+   },500)
+  },[])
   return (
     <div>
       <p>Skills</p>
@@ -11,6 +19,11 @@ const Skills = (props: SkillProps) => {
           <li key={skill}>{skill}</li>
         ))}
       </ul>
+      {isLogin ? (
+        <button onClick={() => setIsLogin(false)}>ログアウト</button>
+      ) : (
+        <button onClick={() => setIsLogin(true)}>ログイン</button>
+      )}
     </div>
   );
 };
