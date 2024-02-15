c927676fa261c16a0836375a087bad9035459213
senna-lang <asrsena3302@gmail.com>
2024-02-15 18:43:08 +0900

    APIテスト

---
+++ src/components/users/Users.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { Users } from './Users';
+
+describe('Users', () => {
+   test('renders correctly', () => {
+     render(<Users />)
+     const textElement = screen.getByText('Users')
+     expect(textElement).toBeInTheDocument()
+   })
+ 
+   test('renders a list of users', async () => {
+     render(<Users />)
+     const users = await screen.findAllByRole('listitem')
+     expect(users).toHaveLength(10)
+   })
+ })
+
+
