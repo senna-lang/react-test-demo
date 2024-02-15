e20d8aa8196bae8c94be88f658c7f3018a11f763
senna-lang <asrsena3302@gmail.com>
2024-02-15 17:49:05 +0900

    ユーザーインタラクションテスト

--- src/components/counter/Counter.test.tsx
+++ src/components/counter/Counter.test.tsx
@@ -22,32 +22,32 @@ describe('Counter', () => {
     expect(countElement).toHaveTextContent('0');
   });
 
-  test('renders a count after clicking the count up button', async () => {
-    const user = userEvent.setup();
-    render(<Counter />);
+  // test('renders a count after clicking the count up button', async () => {
+  //   const user = userEvent.setup();
+  //   render(<Counter />);
 
-    const incrementButton = screen.getByRole('button', {
-      name: 'count up',
-    });
-    await user.click(incrementButton);
+  //   const incrementButton = screen.getByRole('button', {
+  //     name: 'count up',
+  //   });
+  //   await user.click(incrementButton);
 
-    const countElement = screen.getByRole('heading');
-    expect(countElement).toHaveTextContent('1');
-  });
+  //   const countElement = screen.getByRole('heading');
+  //   expect(countElement).toHaveTextContent('1');
+  // });
 
-  test('renders a count 10 after clicking the set button', async () => {
-    const user = userEvent.setup();
-    render(<Counter />);
+  // test('renders a count 10 after clicking the set button', async () => {
+  //   const user = userEvent.setup();
+  //   render(<Counter />);
 
-    const amountInput = screen.getByRole('spinbutton');
-    await user.type(amountInput, '5');
-    expect(amountInput).toHaveValue(5);
+  //   const amountInput = screen.getByRole('spinbutton');
+  //   await user.type(amountInput, '5');
+  //   expect(amountInput).toHaveValue(5);
 
-    const setButton = screen.getByRole('button', {
-      name: 'Set',
-    });
-    await user.click(setButton);
-    const countElement = screen.getByRole('heading');
-    expect(countElement).toHaveTextContent('5');
-  });
+  //   const setButton = screen.getByRole('button', {
+  //     name: 'Set',
+  //   });
+  //   await user.click(setButton);
+  //   const countElement = screen.getByRole('heading');
+  //   expect(countElement).toHaveTextContent('5');
+  // });
 });
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
+import { useCounter } from '../../hooks/useCounter';
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
   const [amount, setAmount] = useState(0);
+  const { count, increment, decrement } = useCounter();
+
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>count up</button>
+      <button onClick={() => increment()}>count up</button>
       <input
         type="number"
         name="amount"
         value={amount}
         onChange={e => setAmount(parseInt(e.target.value))}
       />
-      <button onClick={() => setCount(amount)}>Set</button>
+      {/* <button onClick={() => decrement()}>Set</button> */}
     </div>
   );
 };
